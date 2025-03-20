53d8704a9ae42b985d8c4c361343cd158f9265c5
Dmitry Strunets <d.strunetss@gmail.com>
2025-03-20 15:51:08 +0300

    [LAB-3 ADD CLASS FOR BULLET]

---
+++ bullet.py
@@ -0,0 +1,22 @@
+import math
+from loading_images import bullet_image
+from temporaries import *
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, position, angle, speed, damage):
+        super().__init__()
+        self.original_image = bullet_image
+        self.image = pygame.transform.rotate(
+            self.original_image, -math.degrees(angle))
+        self.rect = self.image.get_rect(center=position)
+        self.speed = speed
+        self.angle = angle
+        self.damage = damage
+
+    def update(self):
+        self.rect.x += self.speed * math.cos(self.angle)
+        self.rect.y += self.speed * math.sin(self.angle)
+
+        if not pygame.Rect(0, 0, game_state.MAP_WIDTH, game_state.MAP_HEIGHT).colliderect(self.rect):
+            self.kill()
